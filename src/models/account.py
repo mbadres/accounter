ea3a8743606f6f197b5a657567baaf920e561d5f
Miena Basta Badres <dev@mbadres.com>
2024-12-08 18:40:03 +0100

    Add the account data.

--- src/models/account.py
+++ src/models/account.py
@@ -8,15 +8,23 @@ class Account:
     def __init__(
         self,
         number: str,
-        type: AccountType,
         description: str,
+        group_number: str,
+        group_description: str,
+        class_number: str,
+        class_description: str,
+        type: AccountType,
         estimated_annual_amount: float,
         cost_center: CostCenter,
         business_area: BusinessArea,
     ) -> None:
         self.number = number
-        self.type = type
         self.description = description
+        self.group_number = group_number
+        self.group_description = group_description
+        self.class_number = class_number
+        self.class_description = class_description
+        self.type = type
         self.estimated_annual_amount = estimated_annual_amount
         self.cost_center = cost_center
         self.business_area = business_area
--- src/models/account_type.py
+++ src/models/account_type.py
@@ -2,9 +2,22 @@ from enum import Enum
 
 
 class AccountType(Enum):
-# Aktickonto = asset account
-# Passivkonto = liability account
     """Type of the account"""
 
-    INCOME = "Einnahme"
-    EXPENSE = "Ausgabe"
+    # Balance Sheet Accounts (Bestandskonten)
+    ASSET_ACCOUNT = "Aktickonto"
+    LIABILITY_ACCOUNT = "Passivkonto"
+
+    # Profit and Loss Accounts / Income Statement Accounts (Erfolgskonten)
+    REVENUE_ACCOUNT = "Ertragskonto"
+    EXPENSE_ACCOUNT = "Aufwandskonto"
+
+    # Accrual Accounts (Abgrenzungskonten)
+    PREPAID_EXPENSES = "Aktive Rechnungsabgrenzung"
+    DEFERRED_INCOME = "Passive Rechnungsabgrenzung"
+
+    # Drawing Accounts (Privatkonten)
+    PROPRIETOR_S_ACCOUNT = "Privatkonto"
+
+    # Suspense Accounts (Zwischenkonten)
+    TRANSIT_ACCOUNT = "Zwischenkonto"
