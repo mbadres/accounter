e86766ede688d937a7a0cf23524467ebf56e670b
Miena Basta Badres <dev@mbadres.com>
2024-12-08 18:47:49 +0100

    WIP Implement the converter.

--- src/tools/converting.py
+++ src/tools/converting.py
@@ -1,9 +1,35 @@
-from pandas import DataFrame
+from typing import Any
 
-from models.daybook import Daybook
+from pandas import concat, DataFrame
+
+from models.record import Record
 
 
-def convert(activities_books: list[DataFrame], cash_books: list[DataFrame]):
-    # TODO Merge the dataframes into one data sorted dataframe.
-    # TODO Convert the new dataframe into a list of accounting records
-    return Daybook()
+def _map(index: int, row: tuple[Any, ...]) -> Record:
+    # TODO Implement.
+    return Record(
+        index,
+        row.date,
+        row.description,
+        row.amount,
+        row.debit_account,
+        row.credit_account,
+        row.reference,
+    )
+
+
+def convert(
+    activities_books: list[DataFrame], cash_books: list[DataFrame]
+) -> list[Record]:
+    # Merge the dataframes into one data sorted dataframe.
+    data = concat(activities_books, ignore_index=True).sort_values("date")
+
+    # Convert the new dataframe into a list of accounting records.
+    records: list[Record] = []
+    index = 0
+    for row in data.itertuples():
+        index += 1
+        record = _map(index, row)
+        records.append(record)
+
+    return records
