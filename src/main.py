50e56e12aac9b4430754f1080abb7b10acb97c57
Miena Basta Badres <dev@mbadres.com>
2025-02-14 01:01:05 +0100

    Add a new loader for JSON files.

--- src/main.py
+++ src/main.py
@@ -3,9 +3,10 @@ from tools.analyzing import analyze
 from tools.converting import convert
 from tools.exporting import export
 from tools.importing import read
+from tools.loading import load
 
 if __name__ == "__main__":
-    # setup/load
+    addresses, corrections, mappings, settings, templates = load()
     activities_books, cash_books = read(path)
     daybook = convert(activities_books, cash_books)
     reports = analyze(daybook)
--- src/tools/loading.py
+++ src/tools/loading.py
@@ -1,11 +1,20 @@
 import json
 
 
-def load(path):
+def load():
     """
-    This functions loads the settings from a JSON file.
+    This functions loads all JSON file.
     """
-    settings = json.load(open(path, "r"))
-    print(settings)
     # for key, value in settings.items():
     # settings[key] = value
+
+    # accounts = json.load(open("data/accounts.json", "r"))
+    addresses = json.load(open("data/addresses.json", "r"))
+    # classes = json.load(open("data/classes.json", "r"))
+    corrections = json.load(open("data/corrections.json", "r"))
+    # groups = json.load(open("data/groups.json", "r"))
+    mappings = json.load(open("data/mappings.json", "r"))
+    settings = json.load(open("data/settings.json", "r"))
+    templates = json.load(open("data/templates.json", "r"))
+
+    return addresses, corrections, mappings, settings, templates
