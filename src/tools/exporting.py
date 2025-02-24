0d36c7aed8fed0120b73e8ad08aa0633aac6e66e
Miena Basta Badres <dev@mbadres.com>
2025-02-24 22:09:29 +0100

    WIP Implement exporting tool.

--- src/tools/exporting.py
+++ src/tools/exporting.py
@@ -3,6 +3,9 @@ import os
 from docx import Document
 from win32com import client
 
+from models.account import Account
+from models.daybook import Daybook
+from models.donor import Donor
 from models.record import Record
 
 
@@ -45,22 +48,36 @@ def _save(doc: Document, file_name: str):
     word.Quit()
 
 
-def _create_daybook_report(daybook):
+def _create_daybook_report(daybook: Daybook):
     doc = Document()
     _add_heading(doc, "Tagebuch")
     _add_records_table(doc, daybook.records)
     _save(doc, "Tagebuch")
 
 
-def _create_donors_report(donors):
-    pass
+def _create_donors_report(donors: [Donor]):
+    doc = Document()
+    file_name = "Spendenbericht"
+    _add_heading(doc, file_name)
+    _save(doc, file_name)
 
 
-def _create_accounts_report(accounts):
-    pass
+def _create_accounts_report(accounts: [Account]):
+    doc = Document()
+    file_name = "Konten"
+    _add_heading(doc, file_name)
+    _save(doc, file_name)
+
+
+def _create_summery_report(accounts: [Account]):
+    doc = Document()
+    file_name = "Finanzbericht"
+    _add_heading(doc, file_name)
+    _save(doc, file_name)
 
 
 def export(report):
     _create_accounts_report(report.accounts)
     _create_daybook_report(report.daybook)
     _create_donors_report(report.donors)
+    _create_summery_report(report.accounts)
