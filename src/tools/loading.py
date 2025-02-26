05e4cf351821d3c134eb47725998c9707d1acf23
Miena Basta Badres <dev@mbadres.com>
2025-02-26 01:24:19 +0100

    Add possibility to gather donations in kind.

--- src/tools/loading.py
+++ src/tools/loading.py
@@ -12,9 +12,10 @@ def load():
     addresses = json.load(open("data/addresses.json", "r"))
     # classes = json.load(open("data/classes.json", "r"))
     corrections = json.load(open("data/corrections.json", "r"))
+    donations_in_kind = json.load(open("data/donations_in_kind.json", "r"))
     # groups = json.load(open("data/groups.json", "r"))
     mappings = json.load(open("data/mappings.json", "r"))
     settings = json.load(open("data/settings.json", "r"))
     templates = json.load(open("data/templates.json", "r"))
 
-    return addresses, corrections, mappings, settings, templates
+    return addresses, corrections, donations_in_kind, mappings, settings, templates
